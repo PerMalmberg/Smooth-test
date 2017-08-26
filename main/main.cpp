7963f0e6ba5f27d2921d7e3d282b7c4e59d6f24e
Per Malmberg <per.malmberg@gmail.com>
2017-08-26 20:46:35 +0200

    First part of MCP23017

--- main/main.cpp
+++ main/main.cpp
@@ -7,6 +7,9 @@
 //#include <smooth/core/io/spi/Master.h>
 #include <smooth/core/io/i2c/Master.h>
 #include <smooth/application/sensor/BME280.h>
+#include <smooth/application/io/MCP23017.h>
+#include <smooth/core/io/Input.h>
+#include <smooth/core/io/Output.h>
 #include "esp_system.h"
 
 
@@ -24,6 +27,7 @@ using namespace std::chrono;
 using namespace smooth::application::network::mqtt;
 using namespace smooth::application::display;
 using namespace smooth::application::sensor;
+using namespace smooth::application::io;
 
 static const std::string mqtt_broker = "192.168.10.247";
 
@@ -32,11 +36,19 @@ class MyApp
           public IEventListener<MQTTData>
 {
     public:
-        MyApp() : Application(tskIDLE_PRIORITY + 1, std::chrono::seconds(1)),
+        MyApp() : Application(tskIDLE_PRIORITY + 1, std::chrono::milliseconds(1000)),
                   mqtt_data("mqtt_data", 10, *this, *this),
                   mqtt("TestMQTT", std::chrono::seconds(30), 4096, tskIDLE_PRIORITY + 1, mqtt_data),
-                  i2c(I2C_NUM_0, GPIO_NUM_25, true, GPIO_NUM_26, true, 100000),
-                  led(GPIO_NUM_5, true)
+                  i2c(I2C_NUM_0, GPIO_NUM_25, true, GPIO_NUM_26, true, 1000000),
+                  i2c_2(I2C_NUM_1, GPIO_NUM_14, true, GPIO_NUM_27, true, 1000000),
+                  led(GPIO_NUM_5, true),
+                  gpb0(GPIO_NUM_15, GPIO_PULLDOWN_ONLY),
+                  gpb1(GPIO_NUM_5, GPIO_PULLDOWN_ONLY),
+                  gpb2(GPIO_NUM_18, GPIO_PULLDOWN_ONLY),
+                  gpb3(GPIO_NUM_23, GPIO_PULLDOWN_ONLY),
+                  gpb4(GPIO_NUM_19, GPIO_PULLDOWN_ONLY),
+                  gpb5(GPIO_NUM_22, GPIO_PULLDOWN_ONLY),
+                  gpb6(GPIO_NUM_21, GPIO_PULLDOWN_ONLY)
         {
         }
 
@@ -62,6 +74,24 @@ class MyApp
                         BME280::StandbyTimeMS::ST_0_5,
                         BME280::FilterCoeff::FC_16));
             }
+
+            mcp23017 = i2c_2.create_device<MCP23017>(0x20);
+            if (mcp23017)
+            {
+                bool present = mcp23017->is_present();
+                bool known = mcp23017->put_device_into_known_state(false);
+                ESP_LOGV("MCP23017 detected", "%d", present);
+                ESP_LOGV("MCP23017 state set", "%d", known);
+                if (present && known)
+                {
+                    // Set all port B I/O as output
+                    mcp23017->configure_ports(0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+                }
+                else
+                {
+                    mcp23017.reset();
+                }
+            }
         }
 
         void event(const MQTTData& event) override
@@ -72,16 +102,27 @@ class MyApp
                 ss << static_cast<char>(b);
             }
 
-            led.clr();
-            delay(milliseconds(200));
-            led.set();
-
             ESP_LOGV("Received", "%s: %s", event.first.c_str(), ss.str().c_str());
         }
 
         void tick() override
         {
             ESP_LOGV("Main", "Free heap: %u", esp_get_free_heap_size());
+
+            if (mcp23017)
+            {
+
+                mask <<= 1;
+                if (mask == 0)
+                {
+                    mask = 1;
+                }
+
+                mcp23017->set_output(MCP23017::Port::B, mask);
+
+                ESP_LOGV("Read", "mask %d, %d %d %d %d %d %d %d", mask, gpb0.read(), gpb1.read(), gpb2.read(),
+                         gpb3.read(), gpb4.read(), gpb5.read(), gpb6.read());
+            }
 
             if (bme280)
             {
@@ -104,19 +145,8 @@ class MyApp
                 }
             }
 
-            if (mqtt.is_connected())
+            if (!mqtt.is_connected())
             {
-                for( int i = 0; i < 3; ++i)
-                {
-                    led.clr();
-                    delay(milliseconds(50));
-                    led.set();
-                }
-            }
-            else
-            {
-                led.clr();
-                delay(milliseconds(500));
                 led.set();
                 delay(milliseconds(500));
                 led.clr();
@@ -127,9 +157,19 @@ class MyApp
         smooth::core::ipc::TaskEventQueue<MQTTData> mqtt_data;
         smooth::application::network::mqtt::MqttClient mqtt;
         smooth::core::io::i2c::Master i2c;
+        smooth::core::io::i2c::Master i2c_2;
         std::unique_ptr<BME280> bme280{};
+        std::unique_ptr<MCP23017> mcp23017;
         std::stringstream ss{};
         smooth::core::io::Output led;
+        uint8_t mask = 1;
+        io::Input gpb0;
+        io::Input gpb1;
+        io::Input gpb2;
+        io::Input gpb3;
+        io::Input gpb4;
+        io::Input gpb5;
+        io::Input gpb6;
 };
 
 extern "C" void app_main()
